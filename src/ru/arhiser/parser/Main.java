2eb920db657aee857aaecd0822af00755ef765b6
Sergey Arkhipov <arhiser@yandex.ru>
2020-02-06 21:06:43 +0300

    Expression parser

---
+++ src/ru/arhiser/parser/Main.java
@@ -0,0 +1,198 @@
+package ru.arhiser.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    /*------------------------------------------------------------------
+     * PARSER RULES
+     *------------------------------------------------------------------*/
+
+//    expr : plusminus* EOF ;
+//
+//    plusminus: multdiv ( ( '+' | '-' ) multdiv )* ;
+//
+//    multdiv : factor ( ( '*' | '/' ) factor )* ;
+//
+//    factor : NUMBER | '(' expr ')' ;
+
+
+    public static void main(String[] args) {
+        String expressionText = "122 - 34 - 3* (55 + 5* (3 - 2)) * 2";
+        List<Lexeme> lexemes = lexAnalyze(expressionText);
+        LexemeBuffer lexemeBuffer = new LexemeBuffer(lexemes);
+        System.out.println(expr(lexemeBuffer));
+    }
+
+    public enum LexemeType {
+        LEFT_BRACKET, RIGHT_BRACKET,
+        OP_PLUS, OP_MINUS, OP_MUL, OP_DIV,
+        NUMBER,
+        EOF;
+    }
+
+    public static class Lexeme {
+        LexemeType type;
+        String value;
+
+        public Lexeme(LexemeType type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        public Lexeme(LexemeType type, Character value) {
+            this.type = type;
+            this.value = value.toString();
+        }
+
+        @Override
+        public String toString() {
+            return "Lexeme{" +
+                    "type=" + type +
+                    ", value='" + value + '\'' +
+                    '}';
+        }
+    }
+
+    public static class LexemeBuffer {
+        private int pos;
+
+        public List<Lexeme> lexemes;
+
+        public LexemeBuffer(List<Lexeme> lexemes) {
+            this.lexemes = lexemes;
+        }
+
+        public Lexeme next() {
+            return lexemes.get(pos++);
+        }
+
+        public void back() {
+            pos--;
+        }
+
+        public int getPos() {
+            return pos;
+        }
+    }
+
+    public static List<Lexeme> lexAnalyze(String expText) {
+        ArrayList<Lexeme> lexemes = new ArrayList<>();
+        int pos = 0;
+        while (pos< expText.length()) {
+            char c = expText.charAt(pos);
+            switch (c) {
+                case '(':
+                    lexemes.add(new Lexeme(LexemeType.LEFT_BRACKET, c));
+                    pos++;
+                    continue;
+                case ')':
+                    lexemes.add(new Lexeme(LexemeType.RIGHT_BRACKET, c));
+                    pos++;
+                    continue;
+                case '+':
+                    lexemes.add(new Lexeme(LexemeType.OP_PLUS, c));
+                    pos++;
+                    continue;
+                case '-':
+                    lexemes.add(new Lexeme(LexemeType.OP_MINUS, c));
+                    pos++;
+                    continue;
+                case '*':
+                    lexemes.add(new Lexeme(LexemeType.OP_MUL, c));
+                    pos++;
+                    continue;
+                case '/':
+                    lexemes.add(new Lexeme(LexemeType.OP_DIV, c));
+                    pos++;
+                    continue;
+                default:
+                    if (c <= '9' && c >= '0') {
+                        StringBuilder sb = new StringBuilder();
+                        do {
+                            sb.append(c);
+                            pos++;
+                            if (pos >= expText.length()) {
+                                break;
+                            }
+                            c = expText.charAt(pos);
+                        } while (c <= '9' && c >= '0');
+                        lexemes.add(new Lexeme(LexemeType.NUMBER, sb.toString()));
+                    } else {
+                        if (c != ' ') {
+                            throw new RuntimeException("Unexpected character: " + c);
+                        }
+                        pos++;
+                    }
+            }
+        }
+        lexemes.add(new Lexeme(LexemeType.EOF, ""));
+        return lexemes;
+    }
+
+    public static int expr(LexemeBuffer lexemes) {
+        Lexeme lexeme = lexemes.next();
+        if (lexeme.type == LexemeType.EOF) {
+            return 0;
+        } else {
+            lexemes.back();
+            return plusminus(lexemes);
+        }
+    }
+
+    public static int plusminus(LexemeBuffer lexemes) {
+        int value = multdiv(lexemes);
+        while (true) {
+            Lexeme lexeme = lexemes.next();
+            switch (lexeme.type) {
+                case OP_PLUS:
+                    value += multdiv(lexemes);
+                    break;
+                case OP_MINUS:
+                    value -= multdiv(lexemes);
+                    break;
+                default:
+                    lexemes.back();
+                    return value;
+            }
+        }
+    }
+
+    public static int multdiv(LexemeBuffer lexemes) {
+        int value = factor(lexemes);
+        while (true) {
+            Lexeme lexeme = lexemes.next();
+            switch (lexeme.type) {
+                case OP_MUL:
+                    value *= factor(lexemes);
+                    break;
+                case OP_DIV:
+                    value /= factor(lexemes);
+                    break;
+                default:
+                    lexemes.back();
+                    return value;
+            }
+        }
+    }
+
+    public static int factor(LexemeBuffer lexemes) {
+        Lexeme lexeme = lexemes.next();
+        switch (lexeme.type) {
+            case NUMBER:
+                return Integer.parseInt(lexeme.value);
+            case LEFT_BRACKET:
+                int value = plusminus(lexemes);
+                lexeme = lexemes.next();
+                if (lexeme.type != LexemeType.RIGHT_BRACKET) {
+                    throw new RuntimeException("Unexpected token: " + lexeme.value
+                            + " at position: " + lexemes.getPos());
+                }
+                return value;
+            default:
+                throw new RuntimeException("Unexpected token: " + lexeme.value
+                        + " at position: " + lexemes.getPos());
+        }
+    }
+
+}
